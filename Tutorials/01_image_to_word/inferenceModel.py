f37712db30fa6c708719c30b84f996b64f448e1c
Gelson Wirti Junior <57411587+gelsonwj@users.noreply.github.com>
2025-03-13 07:34:29 -0400

    .

--- Tutorials/01_image_to_word/inferenceModel.py
+++ Tutorials/01_image_to_word/inferenceModel.py
@@ -27,11 +27,11 @@ if __name__ == "__main__":
     from tqdm import tqdm
     from mltu.configs import BaseModelConfigs
     
-    configs = BaseModelConfigs.load("Models/1_image_to_word/202211270035/configs.yaml")
+    configs = BaseModelConfigs.load("Models/1_image_to_word/202503121444/configs.yaml")
 
     model = ImageToWordModel(model_path=configs.model_path, char_list=configs.vocab)
 
-    df = pd.read_csv("Models/1_image_to_word/202211270035/val.csv").dropna().values.tolist()
+    df = pd.read_csv("Models/1_image_to_word/202503121444/val.csv").dropna().values.tolist()
 
     accum_cer = []
     for image_path, label in tqdm(df[:20]):
